3e6f487bb04181ad52d806f07d44c713954e6b6f
Leila Baneshi <baneshi.leila@gmail.com>
2020-12-22 01:18:58 +0100

    set the header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMEMO295UCEtxF7PakYQGb4SItIj3yiro",
+    authDomain: "linkdin-clone-3657f.firebaseapp.com",
+    projectId: "linkdin-clone-3657f",
+    storageBucket: "linkdin-clone-3657f.appspot.com",
+    messagingSenderId: "250725401060",
+    appId: "1:250725401060:web:4a55d7333d0b98ea9147f1",
+    measurementId: "G-BJE766L0WD"
+};
