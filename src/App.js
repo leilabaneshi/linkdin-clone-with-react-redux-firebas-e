a5d210abced11a22bc6e52fd9538c7bdf98feb2e
Leila Baneshi <baneshi.leila@gmail.com>
2020-12-25 15:01:02 +0100

    create redux store

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import './App.css';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
+
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-
-      {/*Header */}
       <Header />
 
-      {/*app body */}
-      <div className="app__body">
+      {!user ? (
+        <Login />
+      ) : (
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+          </div>
+        )}
 
-
-        {/*sidebar */}
-        <Sidebar />
-
-        {/*feed */}
-        <Feed />
-        {/*widgets */}
-      </div>
     </div>
   );
 }
